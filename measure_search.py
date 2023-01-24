26b5949c81146f2c86b06e41d408938882a68287
KiyoshiMu <mooyewtsing@gmail.com>
2023-01-24 15:25:54 -0500

    add f1

--- measure_search.py
+++ measure_search.py
@@ -73,6 +73,7 @@ MARKS = ["Attention Pooling", "Average Pooling",]
 
 def measure(base_dir:Path):
     df_search_raw = defaultdict(list)
+    df_f1_micro_raw = defaultdict(list)
     for trial in range(5):
         marks = MARKS
         dst_dir = base_dir / f"trial{trial}"
@@ -101,7 +102,15 @@ def measure(base_dir:Path):
                 query_labels,
                 reference_labels,
             )
+            f1_micro = cal_micro_f1(
+                query,
+                reference,
+                query_labels,
+                reference_labels,
+            )
+
             df_search_raw[mark].append(ret["mean_average_precision"])
+            df_f1_micro_raw[mark].append(f1_micro)
             
             if idx == len(marks) - 1:
                 random_query = np.random.rand(*query.shape)
@@ -113,12 +122,24 @@ def measure(base_dir:Path):
                     reference_labels,
                 )
                 df_search_raw["Random"].append(random_ret["mean_average_precision"])
+                f1_micro = cal_micro_f1(
+                    random_query,
+                    random_reference,
+                    query_labels,
+                    reference_labels,
+                )
+                df_f1_micro_raw["Random"].append(f1_micro)
+                
     df_search = pd.DataFrame(df_search_raw)
     df_search = pd.melt(df_search, var_name="Agg Method", value_name="mAP@10")
-    return df_search
+    
+    df_f1_micro = pd.DataFrame(df_f1_micro_raw)
+    df_f1_micro = pd.melt(df_f1_micro, var_name="Agg Method", value_name="Micro F1")
+    return df_search, df_f1_micro
 
 def step(lab_dir:Path):
-    out = []
+    out_search = []
+    out_f1 = []
     base_dirs = list(chain((p for p in lab_dir.iterdir() if p.is_dir())))
     print(base_dirs)
     order = {"neg": 2, "pos": 0, "pos_neg": 1}
@@ -133,95 +154,25 @@ def step(lab_dir:Path):
             _key = "Without K-Means"
         else:
             raise ValueError("Unknown name")
-        part_df = measure(base_dir)
-        part_df["Setting"] = _key
-        out.append(part_df)
-    df = pd.concat(out)
-    df.to_csv(lab_dir / "search_quality.csv", index=False)
+        part_search_df, part_f1_df = measure(base_dir)
+        part_search_df["Setting"] = _key
+        part_f1_df["Setting"] = _key
+        out_search.append(part_search_df)
+        out_f1.append(part_f1_df)
+    search_df = pd.concat(out_search)
+    search_df.to_csv(lab_dir / "search_quality.csv", index=False)
+    
+    f1_df = pd.concat(out_f1)
+    f1_df.to_csv(lab_dir / "f1_micro.csv", index=False)
     
     for mark in MARKS + ["Random"]:
-        fig = box_plot(df.loc[df['Agg Method'] == mark], x="Setting", y="mAP@10")
-        fig.write_image(lab_dir / f"{mark} search quality K.jpg", scale=3)
+        search_fig = box_plot(search_df.loc[search_df['Agg Method'] == mark], x="Setting", y="mAP@10")
+        search_fig.write_image(lab_dir / f"{mark} search quality K.jpg", scale=3)
 
+        f1_fig = box_plot(f1_df.loc[f1_df['Agg Method'] == mark], x="Setting", y="Micro F1")
+        f1_fig.write_image(lab_dir / f"{mark} f1 micro K.jpg", scale=3)
+        
 if __name__ == "__main__":
-    # from collections import defaultdict
-
-    # df_search_raw = defaultdict(list)
-    # df_f1_micro_raw = defaultdict(list)
     lab_dirs = [Path("lab_dense"), Path("lab_vit"), Path("lab_denseK")]
     for lab_dir in lab_dirs:
         step(lab_dir)
-    # for trial in range(5):
-    #     marks = ["Hopfield on Cell Bags",  "rHCT", "AvgPooling on Cell Bags",]
-    #     for idx, mark in enumerate(marks) :
-    #         if mark == "Hopfield on Cell Bags":
-    #             train_pkl_p = f"{base}/trial{trial}/train{trial}_pool.pkl"
-    #             val_pkl_p = f"{base}/trial{trial}/val{trial}_pool.pkl"
-    #         elif mark == "AvgPooling on Cell Bags":
-    #             train_pkl_p = f"{base}/trial{trial}/train_avg{trial}_pool.pkl"
-    #             val_pkl_p = f"{base}/trial{trial}/val_avg{trial}_pool.pkl"
-    #         elif mark == "rHCT":
-    #             train_pkl_p = f"{base}/trial{trial}/train{trial}_hct.pkl"
-    #             val_pkl_p = f"{base}/trial{trial}/val{trial}_hct.pkl"
-    #         else:
-    #             raise ValueError("Unknown mark")
-    #         train = pkl_load(train_pkl_p)
-    #         val = pkl_load(val_pkl_p)
-    #         train_label = [simplify_label(l) for l in train["labels"]]
-    #         val_label = [simplify_label(l) for l in val["labels"]]
-    #         le = LabelEncoder()
-    #         reference_labels = le.fit_transform(train_label)
-    #         query_labels = le.transform(val_label)
-    #         reference = train["embed_pool"]
-    #         query = val["embed_pool"]
-    #         ret = cal_search_quality(
-    #             query,
-    #             reference,
-    #             query_labels,
-    #             reference_labels,
-    #         )
-
-    #         f1_micro = cal_micro_f1(
-    #             query,
-    #             reference,
-    #             query_labels,
-    #             reference_labels,
-    #         )
-
-    #         df_search_raw[mark].append(ret["mean_average_precision"])
-    #         df_f1_micro_raw[mark].append(f1_micro)
-            
-    #         # generate random baseline
-    #         if idx == len(marks) - 1:
-    #             random_query = np.random.rand(*query.shape)
-    #             random_reference = np.random.rand(*reference.shape)
-    #             random_ret = cal_search_quality(
-    #                 random_query,
-    #                 random_reference,
-    #                 query_labels,
-    #                 reference_labels,
-    #             )
-    #             df_search_raw["Random"].append(random_ret["mean_average_precision"])
-    #             f1_micro = cal_micro_f1(
-    #                 random_query,
-    #                 random_reference,
-    #                 query_labels,
-    #                 reference_labels,
-    #             )
-    #             df_f1_micro_raw["Random"].append(f1_micro)
-                
-    # df_f1 = pd.DataFrame(df_f1_micro_raw)
-    # print("df_f1", df_f1.agg(["mean", "std"]))
-    
-    # df_search = pd.DataFrame(df_search_raw)
-    # print("df_search",df_search.agg(["mean", "std"]))
-    # # export to latex with 2 decimal places
-    # df_search.to_latex(f"{base}/search_quality.tex", float_format="%.3f")
-    # # use plotly boxplot to visualize
-    
-    # df_search = pd.melt(df_search, var_name="method", value_name="mAP@10")
-    # fig = box_plot(df_search, x="method", y="mAP@10")
-    # # export to pdf
-    # fig.write_image(f"{base}/search_quality.pdf")
-
-    # df_search.to_csv(f"{base}/search_quality.csv")
--- stats.py
+++ stats.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from pathlib import Path
-def step(p, mark=None):
+def step(p, mark=None, metric="mAP@10"):
     df = pd.read_csv(p)
     # group by Agg Method, calculate the mean, std between different Setting
     groups = df.groupby(["Agg Method", "Setting"]).agg(["mean", "std"])
     # remove the column levels
     groups.columns = groups.columns.droplevel(1)
     groups = groups.reset_index()
-    groups["mAP@10"] = [f"{v:.3f}±{s:.3f}" for v, s in zip(groups.iloc[:, 2],groups.iloc[:, 3])]
+    groups[metric] = [f"{v:.3f}±{s:.3f}" for v, s in zip(groups.iloc[:, 2],groups.iloc[:, 3])]
     groups = groups.iloc[:, 0: 3]
-    groups = groups.sort_values(by="mAP@10", ascending=False)
+    groups = groups.sort_values(by=metric, ascending=False)
     if mark is not None:
         groups["Extraction"] = mark
     # groups.to_latex(Path(p).parent / "search_quality.tex", index=False)
@@ -19,9 +19,12 @@ def step(p, mark=None):
 if __name__ == "__main__":
     labs = ["lab_vit", "lab_denseK", "lab_dense"]
     marks = ["ViT", "KimiaNet", "DenseNet"]
-    out = [step(Path(lab) / "search_quality.csv", mark) for lab, mark in zip(labs, marks)]
-    df = pd.concat(out)
-    # sort the columns to [Extraction, Agg Method, Setting, mAP@10]
-    df = df[["Extraction", "Agg Method", "Setting", "mAP@10"]]
-    df.sort_values(by="mAP@10", ascending=False, inplace=True)
-    df.to_latex("search_quality.csv", index=False)
+    names = ["search_quality.csv", "f1_micro.csv"]
+    metrics  = ["mAP@10", "Micro F1"]
+    for name, metric in zip(names, metrics):
+        out = [step(Path(lab) / name, mark, metric=metric) for lab, mark in zip(labs, marks)]
+        df = pd.concat(out)
+        # sort the columns to [Extraction, Agg Method, Setting, metric]
+        df = df[["Extraction", "Agg Method", "Setting", metric]]
+        df.sort_values(by=metric, ascending=False, inplace=True)
+        df.to_latex(f"{name[:-4]}.lax", index=False)
